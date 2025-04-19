e9c12862734be02676500478f20bb0f91a026ba4
Masterkimaru <themasterskimaru@gmail.com>
2025-04-19 14:34:52 +0300

    new changes for vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    // Allow the ngrok host
-    allowedHosts: [
-      "42d2-102-215-76-178.ngrok-free.app",
-      // Add more hosts if needed (e.g., "*.ngrok-free.app")
-    ],
-    // Optional: Disable or configure HMR (Hot Module Replacement) for ngrok
-    hmr: {
-      clientPort: 443, // Helps with WebSocket connection via ngrok
-    },
-  },
 })
