98b6f4966ce69a312f9bacfd0cb49b6275080ac1
Masterkimaru <themasterskimaru@gmail.com>
2025-04-19 14:23:31 +0300

    use of dotenv

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // src/services/api.ts
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000'; // or your deployed Flask URL
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL // or your deployed Flask URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
